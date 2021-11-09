3d49640233c29f3bf53df9d1749a2936a082f379
gabriel <gabrielfca222@gmail.com>
2021-11-09 09:26:59 -0300

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8203ca360daf4e04d6fa77749c11306e08d0e468',
+        'name' => 'dev/lottery',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dev/lottery' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8203ca360daf4e04d6fa77749c11306e08d0e468',
+            'dev_requirement' => false,
+        ),
+    ),
+);
